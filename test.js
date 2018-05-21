4965f50d80774f5dd721e9d361d2ac2f3a496a2d
quip550@gmail.com Chi <quip550@gmail.com>
2018-05-21 15:58:55 +0000

    automatically backed up by learn

--- test.js
+++ test.js
@@ -2,3 +2,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array.shift();
 }
 
+var a = ['1','2','3']
+destructivelyRemoveElementFromBeginningOfArray(a)
+
+console.log(a)
