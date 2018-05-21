978b611ddf50b0c7d3c72f21d550e2a4623f8542
quip550@gmail.com Chi <quip550@gmail.com>
2018-05-21 15:10:22 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,5 +1,15 @@
 var cities = ["New York", "San Francisco"]
  
-var cities2 = [...cities, "Philadelphia"]
+var cities2 = "Philadelphia"
 
 console.log(cities2)
+
+
+function addcities(array, element) {
+  var newarray = [...array, `${element}`];
+  console.log(newarray);
+}
+
+
+addcities(cities,cities2)
+
